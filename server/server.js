bf8b80141eb23f8ebe7831e6b080218372bd927b
Ian Outterside <ian.outterside@ecommnet.co.uk>
2015-03-07 20:55:02 +0000

    Axel downloader sort of working

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-  var axel = new AxelDownloader('ian', true);
+  var axel = new AxelDownloader('ian');
 
   Meteor.methods({
     addAxelJob: function() {
@@ -10,37 +10,16 @@
       };
 
       axel.download('https://ianoshorty:d4yl1ght0169@put.io/download/279905001', {
-          'file':'1.mp4',
-          'cwd':'/Users/ianoshorty/Downloads'
-        }, 
-        function(status, response){
-
-          if (status === 1) {
-
-            // /\[\s*([0-9]{1,3})%\].*\[\s*([0-9]+\.[0-9]+[A-Z][a-zA-Z]\/s)\]\s*\[([0-9]+:[0-9]+)\]/i
-
-            var re = /\[\s*([0-9]{1,3})%\].*\[\s*([0-9]+\.[0-9]+[A-Z][a-zA-Z]\/s)\]\s*\[([0-9]+:[0-9]+)\]/i; 
-            var str = response;
-            var m;
-            
-            while ((m = re.exec(str)) != null) {
-                if (m.index === re.lastIndex) {
-                    re.lastIndex++;
-                }
-                
-                //console.log('percentage:' + m[1]);
-                //console.log('speed:' + m[2]);
-                //console.log('ttl:' + m[3]);
-                job.percentage = m[1];
-                job.speed = m[2];
-                job.ttl = m[3];
-            }        
-          }     
+        'file':'1.mp4',
+        'cwd':'/Users/ianoshorty/Downloads'
       });
     }, 
 
     pauseAllDownloads: function() {
       
-      axel.pauseAll(); 
+      var status = axel.downloadStatus();
+      //axel.pauseAll(); 
+
+      console.log(status);
     }
   });  
